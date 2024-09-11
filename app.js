a5de44bff4d9daa432e387708eaed175390c4abe
hayde26904 <haydenfrobenius750@gmail.com>
2024-09-10 21:57:06 -0400

    creating new users worky! NOT DONE YET

--- app.js
+++ app.js
@@ -1,131 +1,180 @@
 const express = require('express');
 const app = express();
 const fs = require('fs')
+const sql = require('sqlite3').verbose();
+const bcrypt = require('bcrypt');
+const { hash } = require('crypto');
 const serv = require('http').Server(app);
 
-const sql = require('sqlite3');
-
 const PORT = 3000;
-const userDataPath = __dirname+'/user-data.json'
-const allowedPfpFileExts = ['jpg','png','webp','jfif'];
+
+const saltRounds = 10;
+const allowedPfpFileExts = ['jpg', 'png', 'webp', 'jfif'];
 
 app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/'));
 app.use(express.urlencoded({ extended: true }));
 serv.listen(PORT);
 
-let db = new sql.Database('db/users.db');
-let users;
+let db = new sql.Database('db/database.db');
 
-fs.readFile(userDataPath, (err, data) => {
-    if (err) throw err;
-    users = JSON.parse(data);
-});
+async function createNewUser(userData) { // just put async everywhere until it works
+    return new Promise(async (resolve, reject) => {
+        let username = userData.username;
+        let pfpFileName = userData.pfpFileName;
+        let hashedPass;
 
-function writeNewUser(userData){
-    users.push(userData);
+        await hashPassword(userData.rawPass).then(hash => {
+            hashedPass = hash;
+        });
 
-    fs.writeFile(userDataPath, JSON.stringify(users), err => {
-        if(err) throw err;
+        let createUserSQL = `INSERT INTO users (username, password, pfpfilename) VALUES ("${username}", "${hashedPass}", "${pfpFileName}");`;
 
-        console.log("Wrote User")
+        db.serialize(() => {
+            db.run(createUserSQL, (err) => {
+                if (err) reject(err);
+                resolve(this.lastID);
+            });
+        });
+
+    });
+
+}
+
+function hashPassword(rawPass) {
+    return new Promise((resolve, reject) => {
+        let hashedPass;
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) reject(err);
+            bcrypt.hash(rawPass, salt, (err, hash) => {
+                if (err) reject(err);
+                hashedPass = hash;
+                resolve(hashedPass);
+            });
+        });
+    });
+
+}
+
+function isUsernameTaken(username) {
+    return new Promise((resolve, reject) => {
+        db.get(`SELECT id FROM users WHERE username = "${username}"`, (err, row) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(row !== undefined);
+        });
     });
 }
 
-function getUser(ip){
-    let i = users.findIndex(entry => entry.ip === ip);
-    if(i === -1){
-        return false;
+function isUsernameValid(username) {
+    let rules = [username.length >= 3];
+
+    for (rule of rules) {
+        if (!rule) return false;
     }
 
-    return users[i];
-}
-
-function userExists(ip){
-    return users.some(entry => entry.ip === ip);
-}
-
-function usernameValid(username){
-    if(!username){
-        console.log("BAD USER");
-        return false;
-    }
 
     return true;
 }
 
-function pfpPathValid(pfpPath){
+function isRawPasswordValid(rawPass) {
+    let rules = [rawPass.length >= 3];
 
-    if(!pfpPath){
+    for (rule of rules) {
+        if (!rule) return false;
+    }
+
+
+    return true;
+}
+
+function isPfpValid(pfpFileName) {
+
+    if (pfpFileName.length === 0) {
         return false;
     }
 
-    let fileExt = pfpPath.split('.');
+    let fileExt = pfpFileName.split('.');
     fileExt = fileExt[fileExt.length - 1];
 
-    if(!allowedPfpFileExts.includes(fileExt)){
+    if (!allowedPfpFileExts.includes(fileExt)) {
         return false;
     }
 
     return true;
 }
 
-function userValid(userData){
+async function isUserDataValid(userData) {
     let error;
-    
+    let usernameTaken;
+    await isUsernameTaken(userData.username).then(isTaken => {
+        usernameTaken = isTaken;
+    });
+
     //I'm a dirty hypocrite
-    if(userExists(userData.ip)){
-        error = "USER EXISTS";
-    } else if(!usernameValid(userData.username)){
+    if (usernameTaken) {
+        error = "USERNAME TAKEN";
+    } else if (!isUsernameValid(userData.username)) {
         error = "INVALID USERNAME";
-    } else if(!pfpPathValid(userData.pfpPath)){
+    } else if (!isRawPasswordValid(userData.rawPass)) {
+        error = "INVALID PASSWORD";
+    } else if (!isPfpValid(userData.pfpFileName)) {
         error = "INVALID PFP";
-    }
+    } 
 
     return error;
 }
 
 const io = require('socket.io')(serv);
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     let ip = socket.handshake.address
 
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
 
     });
 });
 
 app.get('/', (req, res) => {
     let ip = req.socket.remoteAddress;
-
-    if(userExists(ip)){
-        res.render('chat', {userData: getUser(ip)});
-    } else {
-        res.render('register');
-    }
+    res.render('register');
 
 });
 
-app.post('/register', (req, res) => {
+app.get('/chat', (req, res) => {
+    res.render('chat', {user: { //FOR TESTING NOT DONE YET
+        username: "CHAT"
+    }});
+});
+
+app.post('/register', async (req, res) => {
     let ip = req.socket.remoteAddress;
     let username = req.body.username;
-    let pfpPath = req.body.pfpPath;
+    let rawPass = req.body.rawPass;
+    let pfpFileName = req.body.pfpFileName;
+
     let userData = {
-        ip: ip,
         username: username,
-        pfpPath: pfpPath
+        rawPass: rawPass,
+        pfpFileName: pfpFileName
     }
+
     console.log(userData);
 
-    let errorCode = userValid(userData);
+    let errorCode = await isUserDataValid(userData);
 
     console.log(errorCode);
-    if(errorCode){
-        res.render('register', {previousAttempt: errorCode});
+    if (errorCode) {
+        res.render('register', { previousAttempt: errorCode });
         console.log("EH I'M ERRORING HERE!");
         return
     }
 
-    writeNewUser(userData);
-    res.render('chat', {user: userData});
+    createNewUser(userData).then(userID => {
+        delete userData.rawPass;
+        userData.id = userID;
+        res.redirect('/chat');
+    });
 });
